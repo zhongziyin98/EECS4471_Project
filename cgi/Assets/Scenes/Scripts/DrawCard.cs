ca4d40dc0d78ac50dc43b42e1fb4faaa17610f7f
dabee15 <48809706+dabee15@users.noreply.github.com>
2019-03-29 17:51:49 -0400

    Gesture+draw card

---
+++ cgi/Assets/Scenes/Scripts/DrawCard.cs
@@ -0,0 +1,46 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawCard : MonoBehaviour
+{
+    private bool isCollidedWithObj1 = false;
+    private bool isCollidedWithObj2 = false;
+    bool draw = false;
+    
+   
+    
+    public void OnCollisionEnter(Collision collision)
+    {
+        
+        if (collision.gameObject.name == "Hands")
+            isCollidedWithObj1 = true;
+       // else if (collision.gameObject.name == "Deck")
+         //   isCollidedWithObj2 = true;
+        if (isCollidedWithObj1)
+            Debug.Log("touch");
+        draw = true;
+    }
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "LoPoly Rigged Hand Right")
+        {
+            isCollidedWithObj1 = false;
+            draw = false;
+        }
+
+    }
+    public void SwipeinStart()
+    {
+
+        
+        if (draw == true)
+        {
+            Debug.Log("draw");
+            isCollidedWithObj1 = false;
+
+            draw = false;
+        }
+    }
+}
