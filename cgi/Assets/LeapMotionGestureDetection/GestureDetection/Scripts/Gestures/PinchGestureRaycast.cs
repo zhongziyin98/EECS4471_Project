4e26376b7d1d861a065af9db32a28db2a36b5f4b
zhongziyin98 <48684737+zhongziyin98@users.noreply.github.com>
2019-04-02 13:50:14 -0400

    pinch gesture raycast
    added Diana's script

---
+++ cgi/Assets/LeapMotionGestureDetection/GestureDetection/Scripts/Gestures/PinchGestureRaycast.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchGestureRaycast : GestureBase
+{
+
+    public EHand m_Hand;
+    DetectionManager.DetectionHand m_DetectHand;
+    public GameObject parentObject;
+    public GameObject testObject;
+
+    void Start()
+    {
+        parentObject = GameObject.Find("LoPoly Rigged Hand Right");
+        testObject = GameObject.Find("Cube");
+    }
+
+    void Update()
+    {
+
+    }
+
+    public override bool Detected()
+    {
+        if (DetectionManager.Get().IsHandSet(m_Hand))
+        {
+            // Detect index finger
+            m_DetectHand = DetectionManager.Get().GetHand(m_Hand);
+            EFinger finger = EFinger.eThumb + 1;
+
+            // Do raycast here
+            if (DetectionManager.Get().GetHand(m_Hand).IsPinching())
+            {
+                RaycastHit hit;
+
+                // Draw ray
+                Debug.DrawRay(m_DetectHand.GetFinger(finger).GetTipPosition(), m_DetectHand.GetFinger(finger).GetFingerDirection() * 1000, Color.white);
+
+                // Draw ray for palm instead
+                //Debug.DrawRay(DetectionManager.Get().GetHand(m_Hand).GetHandPosition(), transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+
+                if (Physics.Raycast(m_DetectHand.GetFinger(finger).GetTipPosition(), m_DetectHand.GetFinger(finger).GetFingerDirection(), out hit))
+                {
+                    if (hit.collider.tag == "Card")
+                    {
+                        Debug.Log("YEET");
+                        //hit.collider.gameObject.transform.parent = parentObject.transform;
+                        testObject.transform.parent = parentObject.transform;
+                        //hit.collider.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                    }
+                }
+            } // End raycast
+
+            return DetectionManager.Get().GetHand(m_Hand).IsPinching();
+        }
+
+        return false;
+    }
+}
