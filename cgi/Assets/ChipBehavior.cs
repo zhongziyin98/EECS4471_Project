337c256d86828394e2f8a1b9c61d2dce297b5b5f
zhongziyin98 <48684737+zhongziyin98@users.noreply.github.com>
2019-04-14 14:37:08 -0400

    chip behavior
    added chip behavior file containing the old GrapChip file.

--- cgi/Assets/ChipBehavior.cs
+++ cgi/Assets/ChipBehavior.cs
@@ -2,18 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GrabChip : MonoBehaviour
+public class ChipBehavior : MonoBehaviour
 {
+    public bool held = false;
+
     bool grab = false;
     public GameObject handObject;
     Vector3 vector;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
     void Update()
     {
+        //GetComponent<Rigidbody>().isKinematic = held;
+
         handObject = GameObject.Find("R_Palm");
         vector = handObject.transform.position;
         vector.y -= 0.1f;
     }
+    
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,17 +48,17 @@ public class GrabChip : MonoBehaviour
             Debug.Log("Grab chip");
 
             transform.parent = handObject.transform;
-            transform.position = vector;
+            //transform.position = vector;
 
             grab = false;
+            held = true; 
         }
     }
 
-    public void GrabinEnd() {
+    public void GrabinEnd()
+    {
 
-        transform.parent = null; 
-
-
+        transform.parent = null;
+        held = false; 
     }
-
 }
