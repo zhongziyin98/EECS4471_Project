f8ddab751ac73205b6aa2cc159a5125803253a35
vice222 <vice222@users.noreply.github.com>
2019-04-15 18:45:15 -0400

    gun

--- cgi/Assets/Pistol.cs
+++ cgi/Assets/Pistol.cs
@@ -9,8 +9,10 @@ public class Pistol : GestureBase
 
     FingerExtendedDetails m_GestureDetail;
 
-    float m_CooldownTime = 0.2f;
+    float m_CooldownTime = 0.4f;
     float m_CoolDownLeft = 0.0f;
+
+    GameObject prefab;
 
     void Start()
     {
@@ -19,6 +21,8 @@ public class Pistol : GestureBase
         m_GestureDetail.bMiddleExtended = false;
         m_GestureDetail.bRingExtended = false;
         m_GestureDetail.bPinkeyExtended = false;
+
+        prefab = Resources.Load("bullet") as GameObject;
     }
 
     void Update()
@@ -37,15 +41,36 @@ public class Pistol : GestureBase
     {
         DetectionManager.DetectionHand detectHand = DetectionManager.Get().GetHand(m_Hand);
 
+        EFinger indexFinger = EFinger.eThumb+1;
+
         if (detectHand.IsSet())
         {
             if (detectHand.CheckWithDetails(m_GestureDetail))
             {
                 var rot = detectHand.GetRotation();
 
-                //Debug.Log(rot);
+                
+                //Debug.Log(di);
 
-                m_CoolDownLeft = m_CooldownTime;
+                if (rot.x > 0.35f && m_CoolDownLeft <= 0.0f )
+                {
+                    m_CoolDownLeft = m_CooldownTime;
+                    GameObject projectile = Instantiate(prefab) as GameObject;
+                    var spawn = detectHand.GetFinger(indexFinger).GetTipPosition();
+                   
+                    
+
+                    projectile.transform.position = spawn ;
+                    Rigidbody rb = projectile.GetComponent<Rigidbody>();
+                    //var di = detectHand.GetFinger(indexFinger).GetFingerDirection();
+                    //di.y -= 0.6f;
+                   // di.z += 0.3f;
+                    //di.x += 0.2f;
+                    //rb.AddForce(Camera.main.transform.forward * 20f, ForceMode.VelocityChange);
+                    rb.velocity = Camera.main.transform.forward * 8;
+                }
+
+                
                 return true;
             }
         }
