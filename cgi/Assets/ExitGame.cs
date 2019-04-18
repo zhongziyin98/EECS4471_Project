ee87c85d269cd75e8f5347513d8597992dba3266
zhongziyin98 <48684737+zhongziyin98@users.noreply.github.com>
2019-04-18 14:52:52 -0400

    exit game imp

--- cgi/Assets/ExitGame.cs
+++ cgi/Assets/ExitGame.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGame : MonoBehaviour
 {
 
     public void Exit() {
         Debug.Log("exiting");
-        Application.Quit();
+        SceneManager.LoadScene("Menu", LoadSceneMode.Single);
     }
 
     
